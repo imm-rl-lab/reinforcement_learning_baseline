61ac362622d0afabf46b482a56d824ffd27e24f2
IMM RL Lab <53929619+imm-rl-lab@users.noreply.github.com>
2022-04-09 11:19:19 +0500

    Update CEM.py

--- Agents/CEM.py
+++ Agents/CEM.py
@@ -1,11 +1,19 @@
+#Cross-Entropy Algorithms for both discrete and continuous action spaces are implemented
+
+
+import torch
+import torch.optim as optim
 import torch.nn.functional as func
+import numpy as np
+from copy import deepcopy
 from Agents.Utilities.LinearTransformations import transform_interval
 
 
 class CEM():
-    def __init__(self, pi_model, pi_model_lr, tau, percentile_param, 
+    def __init__(self, pi_model, noise, pi_model_lr, tau, percentile_param, 
                  learning_iter_per_fit):
         self.pi_model = pi_model
+        self.noise = noise
         self.tau = tau
         self.percentile_param = percentile_param
         self.learning_iter_per_fit = learning_iter_per_fit
@@ -14,7 +22,7 @@ class CEM():
     
     def fit(self, sessions):
         #get elite states and actions
-        elite_states, elite_actions, elite_session_n = self.get_elite_sessions(sessions)
+        elite_states, elite_actions, elite_session_n = self.get_elite_states_and_actions(sessions)
         
         #learn
         if 0 < elite_session_n < len(sessions):
@@ -50,11 +58,17 @@ class CEM():
                 param.data.copy_(self.tau * param + (1 - self.tau) * copy_param)
         return None
     
+    def reset(self):
+        self.noise.reset()
+        self.noise.reduce()
+        return None
+
+
 class CEM_Discrete(CEM):
-    def __init__(self, pi_model, noise, tau=1e-2, percentile_param=70, 
+    def __init__(self, pi_model, noise, pi_model_lr, tau=1e-2, percentile_param=70, 
                  learning_iter_per_fit=16, greedy_policies=False):
 
-        super().__init__(pi_model, pi_model_lr, tau, 
+        super().__init__(pi_model, noise, pi_model_lr, tau, 
                          percentile_param, learning_iter_per_fit)
         
         self.noise = noise
@@ -65,7 +79,7 @@ class CEM_Discrete(CEM):
         if np.random.uniform() < self.noise.threshold:
             return self.noise.get()
         else:
-            probs = self.get_probs(model, state)
+            probs = self.get_probs(self.pi_model, state)
             if self.greedy_policies:
                 action = np.argmax(probs.detach().data.numpy())
             else:
@@ -89,28 +103,28 @@ class CEM_Discrete(CEM):
 class CEM_Continuous(CEM):
     
     def __init__(self, action_min, action_max, pi_model, noise, 
-                 tau=1e-2, percentile_param=70, learning_iter_per_fit=16):
+                 pi_model_lr=1e-3, tau=1e-2, percentile_param=70, learning_iter_per_fit=16):
         
-        super().__init__(pi_model, pi_model_lr, tau, 
+        super().__init__(pi_model, noise, pi_model_lr, tau, 
                          percentile_param, learning_iter_per_fit)
         
         self.action_min = action_min
         self.action_max = action_max
         return None
         
-    def get_u_action(self, state):
+    def get_action(self, state):
         action = self.pi_model(state).detach().numpy() + self.noise.get()
         action = self.transform_interval(action)
-        return np.clip(action, self.u_action_min, self.u_action_max)
+        return np.clip(action, self.action_min, self.action_max)
     
-    def transform_interval(action):
+    def transform_interval(self, action):
         return transform_interval(action, self.action_min, self.action_max)
     
     def update_policy(self, elite_states, elite_actions):
         #get loss
         elite_actions = torch.FloatTensor(elite_actions)
-        pred_actions = transform_interval(self.pi_model(elite_states))
-        loss = torch.mean((predicted_action - elite_actions) ** 2)
+        pred_actions = self.transform_interval(self.pi_model(elite_states))
+        loss = torch.mean((pred_actions - elite_actions) ** 2)
         
         #learn
         self.update_model(self.pi_model, self.optimizer, loss)
