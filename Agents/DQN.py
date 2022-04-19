000a60243b7e5a458bafbb550f91e32ec83a3bb3
IMM RL Lab <53929619+imm-rl-lab@users.noreply.github.com>
2022-04-19 21:47:57 +0500

    Update DQN.py

--- Agents/DQN.py
+++ Agents/DQN.py
@@ -4,9 +4,10 @@ import torch
 from collections import deque
 from copy import deepcopy
 
+
 class DQN():
     def __init__(self, q_model, noise, q_model_lr=1e-3, gamma=1, 
-                 batch_size=128, tau=1e-3, memory_len=5000):
+                 batch_size=64, tau=1e-2, memory_len=50000):
         self.q_model = q_model
         self.noise = noise
         self.gamma = gamma
@@ -58,4 +59,37 @@ class DQN():
     def reset(self):
         self.noise.reset()
         self.noise.reduce()
+        return None
+
+
+class DDQN(DQN):
+    def __init__(self, q_model, noise, q_model_lr=1e-3, gamma=1, 
+                 batch_size=64, tau=1e-2, memory_len=50000):
+        '''Double DQN'''
+        super().__init__(q_model, noise, q_model_lr=1e-3, gamma=1, 
+                 batch_size=64, tau=1e-2, memory_len=50000)
+        return None
+                         
+    def fit(self, state, action, reward, done, next_state):
+        #add to memory
+        self.memory.append([state, action, reward, done, next_state])
+
+        if len(self.memory) >= self.batch_size:
+            #get batch
+            batch = random.sample(self.memory, self.batch_size)
+            states, actions, rewards, dones, next_states = map(np.array, zip(*batch))
+            
+            #get targets
+            q_values = self.q_model(states)
+            targets = q_values.clone().detach()
+            next_q_values = self.q_model(next_states)
+            next_q_target_values = self.q_target_model(next_states).data.numpy()
+            for i in range(self.batch_size):
+                next_max_action = torch.argmax(next_q_values[i])
+                targets[i][actions[i]] = rewards[i] + self.gamma * (1 - dones[i]) * next_q_target_values[i][next_max_action]
+            
+            #learn q_model
+            loss = torch.mean((targets.detach() - q_values) ** 2)
+            self.update_target_model(self.q_target_model, self.q_model, self.optimizer, loss)
+
         return None
