bddc50aee742c9e911cc37f05f8f1857c59307ce
IMM RL Lab <53929619+imm-rl-lab@users.noreply.github.com>
2022-04-19 21:45:40 +0500

    Update ContinuousAgentMaker.py

--- Agents/Utilities/ContinuousAgentMakers/ContinuousAgentMaker.py
+++ Agents/Utilities/ContinuousAgentMakers/ContinuousAgentMaker.py
@@ -6,44 +6,39 @@ class array_with_i(list):
         return None
 
 
-class ContinuousAgentMaker():
+def ContinuousAgentMaker(DiscreteAgent):
     '''The class convert an agent for a discrete action space 
     to the agent for the continuous action space'''
-    def __init__(self, DiscreteAgent):
-        self.DiscreteAgent = DiscreteAgent
-        return self.get_continuous_agent()
-        
-    def get_continuous_agent(self):
 
-        class ContinuousAgent(self.DiscreteAgent):
-            def __init__(self, *args, action_values, **kwargs):
-                super().__init__(*args, **kwargs)
-                self.action_values = action_values
+    class ContinuousAgent(DiscreteAgent):
+        def __init__(self, *args, action_values, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.action_values = action_values
 
-                if super().fit.__code__.co_argcount==2:
-                    self.fit = self.fit_by_sessions
-                elif super().fit.__code__.co_argcount==6:
-                    self.fit = self.fit_by_fives
+            if super().fit.__code__.co_argcount==2:
+                self.fit = self.fit_by_sessions
+            elif super().fit.__code__.co_argcount==6:
+                self.fit = self.fit_by_fives
 
-                return None
+            return None
 
-            def get_action(self, state):
-                action_i = super().get_action(state)
-                action = array_with_i(self.action_values[action_i])
-                action.i = action_i
-                return action
+        def get_action(self, state):
+            action_i = super().get_action(state)
+            action = array_with_i(self.action_values[action_i])
+            action.i = action_i
+            return action
 
-            def fit_by_sessions(self, sessions):
-                for session in sessions:
-                    actions = []
-                    for action in session['actions']:
-                        actions.append(action.i)
-                    session['actions'] = actions
-                super().fit(sessions)
-                return None
+        def fit_by_sessions(self, sessions):
+            for session in sessions:
+                actions = []
+                for action in session['actions']:
+                    actions.append(action.i)
+                session['actions'] = actions
+            super().fit(sessions)
+            return None
 
-            def fit_by_fives(self, state, action, reward, done, next_action):
-                super().fit(state, action.i, reward, done, next_action)
-                return None
+        def fit_by_fives(self, state, action, reward, done, next_action):
+            super().fit(state, action.i, reward, done, next_action)
+            return None
 
-        return ContinuousAgent
+    return ContinuousAgent
