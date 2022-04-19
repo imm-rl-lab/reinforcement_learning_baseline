7a3fa694bd861cb8d0591a063579aedce84fb6e5
IMM RL Lab <53929619+imm-rl-lab@users.noreply.github.com>
2022-04-19 21:45:11 +0500

    Create ContinuousAgentMaker.py

---
+++ Agents/Utilities/ContinuousAgentMakers/ContinuousAgentMaker.py
@@ -0,0 +1,49 @@
+class array_with_i(list):
+    'class of float number with inner attribute i'
+    def __init__(self, v):
+        super().__init__(v)
+        self.i = None
+        return None
+
+
+class ContinuousAgentMaker():
+    '''The class convert an agent for a discrete action space 
+    to the agent for the continuous action space'''
+    def __init__(self, DiscreteAgent):
+        self.DiscreteAgent = DiscreteAgent
+        return self.get_continuous_agent()
+        
+    def get_continuous_agent(self):
+
+        class ContinuousAgent(self.DiscreteAgent):
+            def __init__(self, *args, action_values, **kwargs):
+                super().__init__(*args, **kwargs)
+                self.action_values = action_values
+
+                if super().fit.__code__.co_argcount==2:
+                    self.fit = self.fit_by_sessions
+                elif super().fit.__code__.co_argcount==6:
+                    self.fit = self.fit_by_fives
+
+                return None
+
+            def get_action(self, state):
+                action_i = super().get_action(state)
+                action = array_with_i(self.action_values[action_i])
+                action.i = action_i
+                return action
+
+            def fit_by_sessions(self, sessions):
+                for session in sessions:
+                    actions = []
+                    for action in session['actions']:
+                        actions.append(action.i)
+                    session['actions'] = actions
+                super().fit(sessions)
+                return None
+
+            def fit_by_fives(self, state, action, reward, done, next_action):
+                super().fit(state, action.i, reward, done, next_action)
+                return None
+
+        return ContinuousAgent
