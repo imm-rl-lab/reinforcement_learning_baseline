f08da6f7d739f546020b7e56064a2674d6822777
IMM RL Lab <53929619+imm-rl-lab@users.noreply.github.com>
2022-05-18 20:06:23 +0500

    Update A2C.py

--- Agents/A2C.py
+++ Agents/A2C.py
@@ -2,18 +2,16 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from Agents.Utilities.Noises import DiscreteUniformNoise
 
 
 class A2C():
-    def __init__(self, state_dim, action_min, action_max, pi_model, v_model, 
-                 gamma=0.99, pi_model_lr=1e-3, v_model_lr=1e-3, entropy_threshold=1, action_space_type='discrete'):
-        self.state_dim = state_dim
+    def __init__(self, action_min, action_max, pi_model, v_model, 
+                 gamma=0.99, pi_model_lr=1e-3, v_model_lr=1e-3, 
+                 entropy_threshold=1, action_space_type='discrete'):
         self.action_min = action_min
         self.action_max = action_max
         self.pi_model = pi_model
         self.v_model = v_model
-        self.noise = DiscreteUniformNoise(2)
         self.gamma = gamma
         self.entropy_threshold = entropy_threshold
         self.action_space_type = action_space_type
@@ -37,56 +35,57 @@ class A2C():
             return torch.distributions.Normal(mean, 0.1)
     
     def get_action(self, state):
-        state = torch.FloatTensor(state)
         dist = self.get_dist(state)
         if self.action_space_type == 'discrete':
-            action = int(dist.sample())
+            return int(dist.sample())
         else:
             action = dist.sample().detach().data.numpy()
-            action = np.clip(action, self.action_min, self.action_max)
-        return action
+            return np.clip(action, self.action_min, self.action_max)
 
+    def fit(self, sessions):
+        #get states, actions, and returns
+        states, actions, returns = [], [], []
+        for session in sessions:
+            states.extend(session['states'][:-1])
+            actions.extend(session['actions'])
+            
+            last_done = session['dones'][-1]
+            last_state = session['states'][-1]
+            last_value = self.v_model(last_state)
+            returns = self.get_returns(session['rewards'], last_done, last_value)
+            returns.extend(returns)
+        
+        #convert actions and returns to tensor
+        if self.action_space_type == 'discrete':
+            actions = torch.LongTensor(actions)
+        if self.action_space_type == 'continuous':
+            actions = torch.FloatTensor(actions)
+        returns = torch.FloatTensor(returns)
+        
+        #get advantages
+        advantages = returns.detach() - self.v_model(states)
+        
+        #get entropy
+        dist = self.get_dist(states)
+        entropy = dist.entropy().mean()
+        
+        #update pi_model
+        pi_loss = - (torch.mean(dist.log_prob(actions) * advantages.detach()) + self.entropy_threshold * entropy)
+        self.pi_optimizer.zero_grad()
+        pi_loss.backward()
+        self.pi_optimizer.step()
+        
+        #update v_model
+        v_loss = torch.mean(advantages ** 2)
+        self.v_optimizer.zero_grad()
+        v_loss.backward()
+        self.v_optimizer.step()
+        
+        return None
+    
     def get_returns(self, rewards, last_done, last_value):
         returns = rewards.copy()
         returns[-1] += self.gamma * (1 - last_done) * last_value
         for i in range(len(rewards) - 2, -1, -1):
             returns[i] += self.gamma * returns[i + 1]
         return returns
-
-    def fit(self, sessions):
-        
-        states, actions, rewards, returns = [], [], [], []
-        for session in sessions:
-            states.extend(session['states'][:-1])
-            actions.extend(session['actions'])
-            rewards.extend(session['rewards'])
-            
-            last_done = session['dones'][-1]
-            last_state = session['states'][-1]
-            last_value = self.v_model(torch.FloatTensor(last_state))
-            returns.extend(self.get_returns(session['rewards'], last_done, last_value))
-        
-        if self.action_space_type == 'discrete':
-            actions = torch.LongTensor(actions)
-        if self.action_space_type == 'continuous':
-            actions = torch.FloatTensor(actions)
-        
-        states = torch.FloatTensor(states)
-        returns = torch.FloatTensor(returns)
-        
-        advantages = returns.detach() - self.v_model(states)
-        
-        dist = self.get_dist(states)
-        entropy = dist.entropy().mean()
-        
-        pi_loss = - (torch.mean(dist.log_prob(actions) * advantages.detach()) + self.entropy_threshold * entropy)
-        self.pi_optimizer.zero_grad()
-        pi_loss.backward()
-        self.pi_optimizer.step()
-        
-        v_loss = torch.mean((returns - self.v_model(states)) ** 2)
-        self.v_optimizer.zero_grad()
-        v_loss.backward()
-        self.v_optimizer.step()
-        
-        return None
