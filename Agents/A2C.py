c5e76ca1b3bfb7155c3d4b74e12293a49e4d1a88
IMM RL Lab <53929619+imm-rl-lab@users.noreply.github.com>
2022-05-21 09:13:52 +0500

    Update A2C.py

--- Agents/A2C.py
+++ Agents/A2C.py
@@ -17,25 +17,33 @@ class A2C():
         self.v_optimizer = torch.optim.Adam(self.v_model.parameters(), v_model_lr)
         
         return None
-    
-    def reset(self):
-        pass
-
-    #need to implement
-    def get_dist(self, states):
-        pass
-
-    #need to implement
-    def fit(self, sessions):
-        pass
 
     #need to implement
     def get_action(self, state):
         pass
 
-    #accepts tensors as input
-    #trains a2c agent
-    def fit_A2C(self, states, actions, returns):
+    #need to implement
+    def get_dist(self, states):
+        pass
+    
+    #train agent
+    def fit(self, sessions):
+        #get states, actions, and returns
+        states, actions, returns = [], [], []
+        for session in sessions:
+            states.extend(session['states'][:-1])
+            actions.extend(session['actions'])
+
+            last_done = session['dones'][-1]
+            last_state = session['states'][-1]
+            last_value = self.v_model(last_state)
+            current_returns = self.get_returns(session['rewards'], last_done, last_value)
+            returns.extend(current_returns)   
+        
+        #convert actions and returns to tensor
+        returns = torch.FloatTensor(returns)
+        actions = self.convert_actions_to_tensor(actions)
+        
         #get advantages
         advantages = returns.detach() - self.v_model(states)
 
@@ -54,85 +62,64 @@ class A2C():
         self.v_optimizer.zero_grad()
         v_loss.backward()
         self.v_optimizer.step()
-
+        
         return None
-    
+
     def get_returns(self, rewards, last_done, last_value):
         returns = rewards.copy()
         returns[-1] += self.gamma * (1 - last_done) * last_value
         for i in range(len(rewards) - 2, -1, -1):
             returns[i] += self.gamma * returns[i + 1]
         return returns
-
-    #parse sessions and returns states, actions and returns lists
-    def get_info_from_sessions(self, sessions):
-        states, actions, returns = [], [], []
-        for session in sessions:
-            states.extend(session['states'][:-1])
-            actions.extend(session['actions'])
-
-            last_done = session['dones'][-1]
-            last_state = session['states'][-1]
-            last_value = self.v_model(last_state)
-            current_returns = self.get_returns(session['rewards'], last_done, last_value)
-            returns.extend(current_returns)
-        return states, actions, returns
+    
+    #need to implement
+    def convert_actions_to_tensor(self, actions):
+        pass
+    
+    def reset(self):
+        return None
 
 
 class A2C_Discrete(A2C):
     def __init__(self, pi_model, v_model,
                  gamma=0.99, pi_model_lr=1e-3, v_model_lr=1e-3,
                  entropy_threshold=1):
+        
         super().__init__(pi_model, v_model, gamma, pi_model_lr, v_model_lr, entropy_threshold)
-
-    def get_dist(self, states):
-        logits = self.pi_model(states)
-        probs = F.softmax(logits, -1)
-        return torch.distributions.Categorical(probs)
+        return None
 
     def get_action(self, state):
         dist = self.get_dist(state)
         return int(dist.sample())
 
-    def fit(self, sessions):
-        #get states, actions, and returns
-        states, actions, returns = self.get_info_from_sessions(sessions)
-
-        #convert actions and returns to tensor
-        actions = torch.LongTensor(actions)
-        returns = torch.FloatTensor(returns)
-
-        #a2c fit
-        self.fit_A2C(states, actions, returns)
-        return None
+    def get_dist(self, states):
+        logits = self.pi_model(states)
+        probs = F.softmax(logits, -1)
+        return torch.distributions.Categorical(probs)
+    
+    def convert_actions_to_tensor(self, actions):
+        return torch.LongTensor(actions)
 
 
 class A2C_Continuous(A2C):
     def __init__(self, action_min, action_max, pi_model, v_model,
                  gamma=0.99, pi_model_lr=1e-3, v_model_lr=1e-3,
                  entropy_threshold=1):
+        
         super().__init__(pi_model, v_model, gamma, pi_model_lr, v_model_lr, entropy_threshold)
         self.action_min = action_min
         self.action_max = action_max
-
-    def get_dist(self, states):
-        mean = self.pi_model(states)
-        sigma = torch.full(mean.size(), 0.1)
-        return torch.distributions.Normal(mean, sigma)
+        return None
 
     def get_action(self, state):
         dist = self.get_dist(state)
         action = dist.sample().detach().data.numpy()
         return np.clip(action, self.action_min, self.action_max)
-
-    def fit(self, sessions):
-        #get states, actions, and returns
-        states, actions, returns = self.get_info_from_sessions(sessions)
-
-        #convert actions and returns to tensor
-        actions = torch.FloatTensor(actions)
-        returns = torch.FloatTensor(returns)
-
-        #a2c fit
-        self.fit_A2C(states, actions, returns)
-        return None
+    
+    def get_dist(self, states):
+        mean = self.pi_model(states)
+        sigma = torch.full(mean.size(), 0.1)
+        return torch.distributions.Normal(mean, sigma)
+    
+    def convert_actions_to_tensor(self, actions):
+        return torch.FloatTensor(actions)
