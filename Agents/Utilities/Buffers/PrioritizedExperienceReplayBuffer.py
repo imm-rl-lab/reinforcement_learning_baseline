197cc8bb7bf3a8e8344f33b347de26982e360488
IMM RL Lab <53929619+imm-rl-lab@users.noreply.github.com>
2022-08-04 11:46:55 +0500

    Create PrioritizedExperienceReplayBuffer.py

---
+++ Agents/Utilities/Buffers/PrioritizedExperienceReplayBuffer.py
@@ -0,0 +1,37 @@
+import numpy as np
+import random
+from collections import deque
+
+
+class PrioritizedExperienceReplayBuffer():
+    def __init__(self, alpha=0.7, epsilon=1e-6, memory_len=100000):
+        self.name = 'PrioritizedExperienceReplayBuffer'
+        self.alpha = alpha
+        self.epsilon = epsilon
+        self.values = deque(maxlen=memory_len)
+        self.priorities = deque(maxlen=memory_len)
+        self.index_batch = None
+        return None
+
+    def append(self, value):
+        priority = max(self.priorities) ** self.alpha if len(self) > 0 else 1
+        self.priorities.append(priority)
+        self.values.append(value)
+        return None
+        
+    def get_batch(self, batch_size):
+        probs = np.array(self.priorities) / sum(self.priorities)
+        self.index_batch = np.random.choice(np.arange(len(self)), size=batch_size, p=probs)
+        return [self.values[i] for i in self.index_batch]
+    
+    def update_priorities(self, delta_batch):
+        for index, delta in zip(self.index_batch, delta_batch):
+            self.priorities[index] = (abs(delta) + self.epsilon) ** self.alpha
+        return None
+    
+    def get_weights(self, beta=0.5):
+        priority_batch = np.array([self.priorities[i] for i in self.index_batch])
+        return (priority_batch / min(self.priorities)) ** beta
+    
+    def __len__(self):
+        return len(self.values)
