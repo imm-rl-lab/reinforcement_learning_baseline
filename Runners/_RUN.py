1601dfc1a3054deed41ba9ca334a0ccccecb40e1
IMM RL Lab <53929619+imm-rl-lab@users.noreply.github.com>
2022-08-17 20:00:23 +0500

    Update _RUN.py

--- Runners/_RUN.py
+++ Runners/_RUN.py
@@ -3,8 +3,9 @@ sys.path.insert(0, os.path.abspath('..'))
 from Utilities.UniversalRunner import UniversalRunner
 
 
-UniversalRunner('CEM_Discrete', {'env_name': ['SimpleControlProblem','Pendulum', 'VanDerPol', 'DubinsCar', 'TargetProblem'], 
-                                 'lr':[1e-2,1e-2],
-                                 'bs':[64,256],
-                                 'tau':[1e-2], 
-                                 'an':[3,5,9]}, parallel=False, attempt_n=5, with_seeds=True)
+UniversalRunner('CVI', {'env_name': ['VanDerPol'], 
+                        'lr':[1e-2],
+                        'bs':[64],
+                        'tau':[1e-2], 
+                        'dt':[0.1]}, 
+                        'en':[100], parallel=False, attempt_n=1)
